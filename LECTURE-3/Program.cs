d8d02d67eac45490fcaf8a087a44bc94dcb1e28e
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2024-11-22 17:19:57 -0500

    Added Lecture 3

---
+++ LECTURE-3/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+/*
+ break;*/
+
+
+int i = 0;
+
+while(i < 10) {
+    Console.WriteLine("(While)The value of i: "+i);
+    if (i==4) {
+        break;
+    }
+    i++;
+}
+
+Console.WriteLine("\n\n");
+
+for (i = 0; i < 10; i++) {
+    Console.WriteLine("(for)The value of i: "+i);
+    if (i==4) {
+        break;
+    }
+}
+
+i = 0;
+while (i < 10) {
+    Console.WriteLine("(While)The value of i: "+i);
+    i++;
+
+    if (i == 4) {
+        continue;
+    }
+    Console.WriteLine("**");
+}
